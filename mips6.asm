0d0fa44903acc78e63bcb6b453899d09ccd59b2c
Anees Asghar <aneesasgharsyed@gmail.com>
2022-06-26 01:09:31 +0100

    clean up

--- mips6.asm
+++
@@ -1,9 +0,0 @@
-.data
-
-.text
-  main: 
-    #i = 0
-    addi $t0, $zero, 0
-    
-    while:
-    	bgt
--- rng.asm
+++
@@ -1,7 +0,0 @@
-.text
-    li $a1, 100  #Here you set $a1 to the max bound.
-    li $v0, 42  #generates the random number.
-    syscall
-    #add $a0, $a0, 100  #Here you add the lowest bound
-    li $v0, 1   #1 print integer
-    syscall
